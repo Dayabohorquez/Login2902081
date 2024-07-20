66a38bff5af1e6ca8367a1f3c8f5c7877ebbf3e3
Dayana Gomez <dayabohorquez93@gmail.com>
2024-07-20 15:33:46 -0500

    Cambios hasta el boton email

--- app/src/main/java/co/edu/sena/login2902081/components/AppComponents.kt
+++ app/src/main/java/co/edu/sena/login2902081/components/AppComponents.kt
@@ -1,15 +1,32 @@
 package co.edu.sena.login2902081.components
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Icon
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import co.edu.sena.login2902081.R
+import co.edu.sena.login2902081.ui.theme.BgColor
+import co.edu.sena.login2902081.ui.theme.Primary
+import co.edu.sena.login2902081.ui.theme.componentShape
 
 @Composable
 fun NormalTextComponent(value: String) {
@@ -17,11 +34,58 @@ fun NormalTextComponent(value: String) {
         text = value,
         modifier = Modifier
             .fillMaxWidth()
-            .heightIn(min = 80.dp),
+            .heightIn(min = 40.dp),
         style = TextStyle(
-            fontSize = 18.sp,
+            fontSize = 24.sp,
             fontWeight = FontWeight.Normal,
             fontStyle = FontStyle.Normal
-        )// TextStyle
-    )// Text
-}// NormalTextComponent
+        ),
+        color = colorResource(id = R.color.colorText),
+        textAlign = TextAlign.Center
+    )
+}
+
+@Composable
+fun HeadingTextComponent(value: String) {
+    Text(
+        text = value,
+        modifier = Modifier
+            .fillMaxWidth()
+            .heightIn(min = 40.dp),
+        style = TextStyle(
+            fontSize = 30.sp,
+            fontWeight = FontWeight.Bold,
+            fontStyle = FontStyle.Normal
+        ),
+        color = colorResource(id = R.color.colorText),
+        textAlign = TextAlign.Center
+    )
+}
+
+@Composable
+fun MyTextFieldComponent(labelValue: String, painterResource: Painter) {
+    val textValue = remember {
+        mutableStateOf("")
+    }
+
+    OutlinedTextField(
+        label = { Text(text = labelValue) },
+        value = textValue.value,
+        onValueChange = {
+            textValue.value = it
+        },
+        leadingIcon = {
+            Icon(painter = painterResource, contentDescription = "")
+        },
+        colors = OutlinedTextFieldDefaults.colors(
+            focusedBorderColor = Primary,
+            focusedLabelColor = Primary,
+            cursorColor = Primary
+        ),
+        keyboardOptions = KeyboardOptions.Default,
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(BgColor)
+            .clip(componentShape.small)
+    )
+}
--- app/src/main/java/co/edu/sena/login2902081/screens/SignUpScreen.kt
+++ app/src/main/java/co/edu/sena/login2902081/screens/SignUpScreen.kt
@@ -1,13 +1,22 @@
 package co.edu.sena.login2902081.screens
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import co.edu.sena.login2902081.R
+import co.edu.sena.login2902081.components.HeadingTextComponent
+import co.edu.sena.login2902081.components.MyTextFieldComponent
 import co.edu.sena.login2902081.components.NormalTextComponent
 
 
@@ -15,10 +24,30 @@ import co.edu.sena.login2902081.components.NormalTextComponent
 fun SignUpScreen() {
     Surface(
         color = Color.White,
-        modifier = Modifier.fillMaxSize()
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.White)
+            .padding(28.dp)
     )
     {
-    NormalTextComponent(value = stringResource(id = R.string.hello))
+        Column(modifier = Modifier.fillMaxSize()) {
+            NormalTextComponent(value = stringResource(id = R.string.hello))
+            HeadingTextComponent(value = stringResource(id = R.string.create_account))
+            Spacer(modifier = Modifier.height(20.dp))
+            MyTextFieldComponent(
+                labelValue = stringResource(id = R.string.first_name),
+                painterResource(id = R.drawable.profile)
+            )
+            MyTextFieldComponent(
+                labelValue = stringResource(id = R.string.last_name),
+                painterResource(id = R.drawable.profile)
+            )
+            MyTextFieldComponent(
+                labelValue = stringResource(id = R.string.email),
+                painterResource(id = R.drawable.message)
+            )
+        }
+
     }// Surface
 } // SignUpScreen
 
